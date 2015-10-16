65667989fd1d115335dd4702dbc89a538791b8d1
fagitfox <goon778@gmail.com>
2015-10-17 01:30:14 +0600

    PAD-19: Add Game script and prefab
     #PAD-19 Fixed

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public Field field;
+    public float speed = 0;
+
+	// Use this for initialization
+	void Awake () {
+        Ball ball = field.balls[0];
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(ball.speed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Debug field, actual speed of the ball
+        Ball ball = field.balls[0];
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        speed = Mathf.Sqrt(Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.y, 2));
+    }
+}
