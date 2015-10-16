517284ea5d158c91295e8d3bf80c7ac0390eeb38
fagitfox <goon778@gmail.com>
2015-10-17 01:25:49 +0600

    PAD-25: Add Border script and prefabs

---
+++ Assets/Scripts/Border.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Border : MonoBehaviour {
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.transform.parent.GetComponent<Collidable>().onCollisionEnter(collision);
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        this.transform.parent.GetComponent<Collidable>().onCollisionStay(collision);
+    }
+    protected void OnCollisionExit2D(Collision2D collision)
+    {
+        this.transform.parent.GetComponent<Collidable>().onCollisionExit(collision);
+    }
+}
