7ad938d15a505b3af000b1726271e64983e537f8
SandakovMM <G0odvinSun@gmail.com>
2015-10-19 23:08:11 +0600

    PAD-13: Add paddle game object.

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : Collidable
+{    
+    public float step = 0.3f;
+ 
+    protected bool Move(int dir)
+    {
+        float shift = step * dir;
+        transform.position = new Vector3(transform.position.x + shift, transform.position.y);
+        return true;
+    }
+
+    void Update()
+    {       
+        int dir = (int)(Input.GetAxisRaw("Horizontal"));     
+
+        if (dir != 0)
+        {
+            Move(dir);
+        }
+    }
+}
