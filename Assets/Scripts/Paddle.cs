e0973ef516ec70804937b8545dff9c5f6f329270
SandakovMM <G0odvinSun@gmail.com>
2015-10-20 20:01:56 +0600

    PAD-17: Создан скрипт игрока и добавлена связь с доской. Часть с обратокой упарвления перенесена в скрипт игрока.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,20 +5,11 @@ public class Paddle : Collidable
 {    
     public float offset = 0.3f;
  
-    protected bool Move(int dir)
+    public bool Move(int dir)
     {
         float step = offset * dir;
         transform.position = new Vector3(transform.position.x + step, transform.position.y);
         return true;
     }
 
-    void Update()
-    {       
-        int dir = (int)(Input.GetAxisRaw("Horizontal"));     
-
-        if (dir != 0)
-        {
-            Move(dir);
-        }
-    }
 }
