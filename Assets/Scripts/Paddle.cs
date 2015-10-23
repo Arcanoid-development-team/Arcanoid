beac213e889dc99a67e26987eca60888752c9b08
Michail Sandakov <michail.sandakov@eltex.loc>
2015-10-23 16:52:40 +0600

    PAD-29: Добавил класс обработки управления.ь

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,6 +7,11 @@ public class Paddle : Collidable
  
     public bool Move(int dir)
     {
+        //Debug.Log("MOve puddle");
+
+        if (0 == dir)
+    		return true;
+    		
         float shift = step * dir;
         transform.position = new Vector3(transform.position.x + shift, transform.position.y);
         return true;
